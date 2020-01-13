8e6377e847a34c214bdc3b460155c2fe5a80d7d7
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2020-01-13 20:22:02 +0700

    Создал функции генерации: случайных символов и строки из этих символов

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
   const number = prompt('Введите число', '10');
+  generateStringFromSymbols(number);
 })
+
+function generateStringFromSymbols(number) {
+  for (let i = 0; i < number; i++) {
+    let rand = generateRandomSymbols();
+    console.log(String.fromCharCode(rand));
+  }
+}
+
+function generateRandomSymbols() {
+  let rand = Math.floor(Math.random() * (90 - 48 + 1)) + 48;
+
+  if (rand > 57 & rand < 65) {
+    return generateRandomSymbols();
+  } else {
+    return rand;
+  }
+}
